17ea9ce39960f358c6154a8ba28e0fef191efd4e
403 <403@DESKTOP-V0T2PPV>
2018-09-18 17:42:57 +0900

    2018-09-18

--- src/day2/Hello.java
+++ src/day2/Hello.java
@@ -1,5 +1,14 @@
 package day2;
-
+/*여러분의 주석 표현
+ *  입니다.
+ *  */
+/**
+ * 
+ * 작성일 : 2018-09-11
+ * 작성자 : 서건희
+ * 설명 : 첫번째 자바 프로그램
+ *
+ */
 public class Hello {
 
 	public static void main(String[] args) {
